022189f7d83ab06a0ea5d8c08c442a8c85f35dc6
Kori Francis <kfrancis@clinicalsupportsystems.com>
2025-11-13 14:55:04 -0500

    clean up

---
+++ tests/CommunityAbp.AspNetZero.FusionCache.Redis.Tests/AbpFusionCacheRedisBackplaneProviderTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityAbp.AspNetZero.FusionCache.Redis.Tests
+{
+    internal class AbpFusionCacheRedisBackplaneProviderTests
+    {
+    }
+}
